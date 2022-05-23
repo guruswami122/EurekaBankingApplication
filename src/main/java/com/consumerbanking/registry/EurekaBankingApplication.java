07221b860f69adfa0e01a05b820d1a2cb83e2f83
guruswami122 <gurusamratscb@gmail.com>
2022-05-23 11:17:16 +0530

    commit

---
+++ src/main/java/com/consumerbanking/registry/EurekaBankingApplication.java
@@ -0,0 +1,15 @@
+package com.consumerbanking.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consumerbanking/registry/EurekaBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumerbanking.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
